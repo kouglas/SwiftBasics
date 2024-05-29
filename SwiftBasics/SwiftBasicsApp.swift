3b3b1d8cb7a4f07857bab340b2969b528cc639bf
Kari <kouglas2@gmail.com>
2024-05-29 17:29:33 -0500

    Initial Commit

---
+++ SwiftBasics/SwiftBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBasicsApp.swift
+//  SwiftBasics
+//
+//  Created by Kari Douglas on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
