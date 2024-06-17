669330475aa3c7c9e40ea4934162b267be423461
Kari <kouglas2@gmail.com>
2024-06-17 17:21:14 -0500

    adding new features

---
+++ SwiftBasics/ToolBarEx.swift
@@ -0,0 +1,39 @@
+//
+//  ToolBarEx.swift
+//  SwiftBasics
+//
+//  Created by Kari Douglas on 6/17/24.
+//
+
+import SwiftUI
+
+struct ToolBarEx: View {
+    var body: some View {
+        NavigationStack {
+            ZStack{
+                Color.purple
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Text("Hey")
+                    .foregroundStyle(.white)
+            }
+            .navigationTitle("Toolbar")
+//            .navigationBarItems(leading:
+//            Image(systemName: "heart.fill"),
+//            trailing: Image(systemName: "gear"))
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image(systemName: "heart.fill")
+                }
+                ToolbarItem(placement: .bottomBar){
+                    Image(systemName: "gear")
+                }
+                
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ToolBarEx()
+}
